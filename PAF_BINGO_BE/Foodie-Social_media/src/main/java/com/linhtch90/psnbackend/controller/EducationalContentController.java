cdd4294b84c1624010105fb2b66e82de6c9fe4fc
JosephDanushan006 <josephdanushan006@gmail.com>
2025-04-24 15:09:55 +0530

    Learning Progress

--- PAF_BINGO_BE/Foodie-Social_media/src/main/java/com/linhtch90/psnbackend/controller/EducationalContentController.java
+++ PAF_BINGO_BE/Foodie-Social_media/src/main/java/com/linhtch90/psnbackend/controller/EducationalContentController.java
@@ -47,6 +47,8 @@ public class EducationalContentController {
         return new ResponseEntity<>(educationalContentService.markAsLearned(request), HttpStatus.OK);
     }
 
+    
+
     @PostMapping("/bookmark")
     public ResponseEntity<ResponseObjectService> bookmarkContent(@RequestBody EducationalContentRequest request) {
         return new ResponseEntity<>(educationalContentService.bookmarkContent(request), HttpStatus.OK);
