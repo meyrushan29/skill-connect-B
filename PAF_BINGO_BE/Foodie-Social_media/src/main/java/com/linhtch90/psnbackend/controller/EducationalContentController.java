58b64db697fc896b106a8b5e6f14ca87da08901f
viththakan1 <viththakan2001@gmail.com>
2025-04-29 18:03:00 +0530

    api integrated

--- PAF_BINGO_BE/Foodie-Social_media/src/main/java/com/linhtch90/psnbackend/controller/EducationalContentController.java
+++ PAF_BINGO_BE/Foodie-Social_media/src/main/java/com/linhtch90/psnbackend/controller/EducationalContentController.java
@@ -70,6 +70,7 @@ public class EducationalContentController {
 
 
 
+
     @PostMapping("/following")
     public ResponseEntity<ResponseObjectService> getFollowingContent(@RequestBody IdObjectEntity userId) {
         return new ResponseEntity<>(educationalContentService.getFollowingContent(userId), HttpStatus.OK);
