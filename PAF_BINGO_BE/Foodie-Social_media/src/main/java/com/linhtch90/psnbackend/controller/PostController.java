39d7a8277ae6db71996baaf0644a9bb49da27259
laxshi24 <laxshisiva@gmail.com>
2025-04-30 08:59:19 +0530

    Unfollow a specific

--- PAF_BINGO_BE/Foodie-Social_media/src/main/java/com/linhtch90/psnbackend/controller/PostController.java
+++ PAF_BINGO_BE/Foodie-Social_media/src/main/java/com/linhtch90/psnbackend/controller/PostController.java
@@ -14,22 +14,24 @@ import org.springframework.web.bind.annotation.*;
 public class PostController {
     @Autowired
     private PostService postService;
+
     @PostMapping("/allposts")
     public ResponseEntity<ResponseObjectService> findAllUsers() {
         System.out.println("hiihi");
         return new ResponseEntity<ResponseObjectService>(postService.findAll(), HttpStatus.OK);
     }
+
     @PostMapping("/insertpost")
     public ResponseEntity<ResponseObjectService> insertPost(@RequestBody PostEntity inputPost) {
         return new ResponseEntity<ResponseObjectService>(postService.insertPost(inputPost), HttpStatus.OK);
     }
+
     @DeleteMapping("/deletepost")
     public ResponseEntity<ResponseObjectService> deletePost(@RequestBody IdObjectEntity inputUserId) {
         System.out.println("Deleting post with ID: " + inputUserId.getId());
         ResponseObjectService response = postService.deleteUserPost(inputUserId);
         return new ResponseEntity<>(response,
-                response.getStatus().equals("success") ?
-                        HttpStatus.OK : HttpStatus.NOT_FOUND);
+                response.getStatus().equals("success") ? HttpStatus.OK : HttpStatus.NOT_FOUND);
     }
 
     @PutMapping("/editpost")
@@ -39,12 +41,13 @@ public class PostController {
 
         return new ResponseEntity<ResponseObjectService>(postService.editUserPost(inputPost), HttpStatus.OK);
     }
-    
+
     @PostMapping("/myposts")
     public ResponseEntity<ResponseObjectService> findPostByUserId(@RequestBody IdObjectEntity inputUserId) {
         return new ResponseEntity<ResponseObjectService>(postService.findPostByUserId(inputUserId), HttpStatus.OK);
     }
 
+    // followingpost
     @PostMapping("/followingposts")
     public ResponseEntity<ResponseObjectService> findPostByFollowing(@RequestBody IdObjectEntity inputUserId) {
         return new ResponseEntity<ResponseObjectService>(postService.findPostByFollowing(inputUserId), HttpStatus.OK);
@@ -52,8 +55,11 @@ public class PostController {
 
     // currently not in use, post is update via comment controller
     // @PutMapping("/updatebycomment")
-    // public ResponseEntity<ResponseObjectService> updateByComment(@RequestBody PostEntity inputPost) {
-    //     return new ResponseEntity<ResponseObjectService>(postService.updatePostByComment(inputPost), HttpStatus.OK);
+    // public ResponseEntity<ResponseObjectService> updateByComment(@RequestBody
+    // PostEntity inputPost) {
+    // return new
+    // ResponseEntity<ResponseObjectService>(postService.updatePostByComment(inputPost),
+    // HttpStatus.OK);
     // }
 
     @PostMapping("/lovepost")
@@ -65,6 +71,7 @@ public class PostController {
     public ResponseEntity<ResponseObjectService> sharePost(@RequestBody DoubleIdObjectEntity doubleId) {
         return new ResponseEntity<ResponseObjectService>(postService.updatePostByShare(doubleId), HttpStatus.OK);
     }
+
     @PostMapping("/savepost")
     public ResponseEntity<ResponseObjectService> savePost(@RequestBody SavePostRequest request) {
         return new ResponseEntity<>(postService.savePost(request), HttpStatus.OK);
