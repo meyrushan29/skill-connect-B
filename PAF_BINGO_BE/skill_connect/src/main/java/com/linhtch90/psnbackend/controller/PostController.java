584895db633b0e25afab9918c92ee722ce362218
meyrushan29 <meyrushan29@gmail.com>
2025-05-11 23:22:41 +0530

    Final_Update

--- PAF_BINGO_BE/skill_connect/src/main/java/com/linhtch90/psnbackend/controller/PostController.java
+++ PAF_BINGO_BE/skill_connect/src/main/java/com/linhtch90/psnbackend/controller/PostController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 //Devlopmment of Backend in Skill Sharing Part
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1")   
 public class PostController {
     @Autowired
     private PostService postService;
